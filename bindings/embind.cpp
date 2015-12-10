284d9b2b2c94c49e78e29c96710ab079eb8995d0
Naoyuki Totani <nyk.totani@gmail.com>
2015-12-11 08:29:45 +0900

    SessionPiece

--- bindings/embind.cpp
+++ bindings/embind.cpp
@@ -5,6 +5,7 @@
 #include "hime/session.h"
 
 using std::make_shared;
+using std::make_unique;
 using std::string;
 using std::shared_ptr;
 using std::unique_ptr;
@@ -15,8 +16,11 @@ using emscripten::enum_;
 using emscripten::pure_virtual;
 using emscripten::wrapper;
 using hime::MasterPiece;
+using hime::OwnedPiece;
+using hime::SessionPiece;
 using hime::Parameter;
 using hime::Planet;
+using hime::Point;
 using hime::Session;
 using hime::SessionContext;
 using hime::SessionContextImpl;
@@ -30,6 +34,10 @@ struct SessionContextWrapper : public wrapper<SessionContext> {
 };
 
 EMSCRIPTEN_BINDINGS(hime) {
+  class_<Point>("Point")
+      .constructor<int, int>()
+      .property("i", &Point::i)
+      .property("j", &Point::j);
   enum_<Planet>("Planet")
       .value("kSun", Planet::kSun)
       .value("kMoon", Planet::kMoon)
@@ -52,14 +60,31 @@ EMSCRIPTEN_BINDINGS(hime) {
       .property("defense", &Parameter::defense)
       .property("resist", &Parameter::resist);
   class_<MasterPiece>("MasterPiece")
-      .constructor<const string&, const string&, Planet,
-          shared_ptr<const Skill>, shared_ptr<const Skill>, Parameter>()
+      .smart_ptr_constructor("shared_ptr<const MasterPiece>",
+          &make_shared<const MasterPiece, const string&, const string&, Planet,
+          shared_ptr<const Skill>, shared_ptr<const Skill>, Parameter>)
       .property("id", &MasterPiece::id)
       .property("name", &MasterPiece::name)
       .property("planet", &MasterPiece::planet)
       .property("active_skill", &MasterPiece::active_skill)
       .property("passive_skill", &MasterPiece::passive_skill)
       .property("param", &MasterPiece::param);
+  class_<OwnedPiece>("OwnedPiece")
+      .smart_ptr_constructor("shared_ptr<const OwnedPiece>",
+          &make_shared<const OwnedPiece,
+          shared_ptr<const MasterPiece>, const string&>)
+      .property("master", &OwnedPiece::master)
+      .property("id", &OwnedPiece::id);
+  /*
+  class_<SessionPiece>("SessionPiece")
+      .smart_ptr_constructor("unique_ptr<SessionPiece>",
+          &make_unique<SessionPiece,
+          shared_ptr<const OwnedPiece>, int, int, Point>)
+      .property("id", &SessionPiece::id);
+      */
+  class_<SessionPiece>("SessionPiece")
+      .constructor<shared_ptr<const OwnedPiece>, int, int, Point>()
+      .property("id", &SessionPiece::id);
   class_<SessionContext>("SessionContext")
       .function("random", &SessionContext::random, pure_virtual())
       .allow_subclass<SessionContextWrapper>("SessionContextWrapper");
