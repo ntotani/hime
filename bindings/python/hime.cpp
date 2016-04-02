37135b4145690e1c9ca60ceb4b97559abffa3ff6
Naoyuki Totani <nyk.totani@gmail.com>
2016-04-02 18:54:22 +0900

    bind master piece

--- bindings/python/hime.cpp
+++ bindings/python/hime.cpp
@@ -1,19 +1,57 @@
 #include <boost/python.hpp>
 #include <string>
+#include "hime/piece.h"
 #include "hime/master.h"
 
 using std::string;
 using std::shared_ptr;
 using boost::python::class_;
+using boost::python::enum_;
 using boost::python::init;
 using boost::python::return_value_policy;
 using boost::python::copy_const_reference;
 using boost::python::make_function;
 using boost::python::register_ptr_to_python;
+using hime::Parameter;
+using hime::MasterPiece;
 using hime::Skill;
 using hime::Master;
+using hime::Planet;
+using hime::PieceAction;
 
 BOOST_PYTHON_MODULE(hime) {
+  enum_<Planet>("Planet")
+    .value("sun", Planet::kSun)
+    .value("moon", Planet::kMoon)
+    .value("mars", Planet::kMars)
+    .value("mercury", Planet::kMercury)
+    .value("jupiter", Planet::kJupiter)
+    .value("venus", Planet::kVenus)
+    .value("saturn", Planet::kSaturn);
+  enum_<PieceAction>("PieceAction")
+    .value("physical", PieceAction::kPhysical)
+    .value("magical", PieceAction::kMagical)
+    .value("heal", PieceAction::kHeal);
+
+  // piece.h
+  class_<Parameter>("Parameter", init<int>()).
+    def(init<int, int, int>())
+    .add_property("power", &Parameter::power)
+    .add_property("defense", &Parameter::defense)
+    .add_property("resist", &Parameter::resist);
+  class_<MasterPiece, boost::noncopyable>("MasterPiece", init<
+      const string&, const string&, Planet, PieceAction,
+      shared_ptr<const Skill>, shared_ptr<const Skill>, Parameter>())
+    .add_property("id", make_function(
+          &MasterPiece::id, return_value_policy<copy_const_reference>()))
+    .add_property("name", make_function(
+          &MasterPiece::name, return_value_policy<copy_const_reference>()))
+    .add_property("planet", &MasterPiece::planet)
+    .add_property("action", &MasterPiece::action)
+    .add_property("active_skill", &MasterPiece::active_skill)
+    .add_property("passive_skill", &MasterPiece::passive_skill)
+    .add_property("param", &MasterPiece::param);
+
   class_<Skill, boost::noncopyable>(
       "Skill", init<string, string, string, int>())
     .add_property("id", make_function(
@@ -24,8 +62,11 @@ BOOST_PYTHON_MODULE(hime) {
           &Skill::desc, return_value_policy<copy_const_reference>()))
     .add_property("rate", &Skill::rate);
   class_<Master, boost::noncopyable>("Master")
-    .def("loadSkill", &Master::LoadSkill)
+    .def("load_piece", &Master::LoadPiece)
+    .def("load_skill", &Master::LoadSkill)
+    .def("piece", &Master::piece)
     .def("skill", &Master::skill);
+  register_ptr_to_python<shared_ptr<const MasterPiece>>();
   register_ptr_to_python<shared_ptr<const Skill>>();
 }
 
--- bindings/python/test.py
+++ bindings/python/test.py
@@ -2,6 +2,8 @@
 import hime
 
 master = hime.Master()
-master.loadSkill("a0,dummy,dummy skill,0")
+master.load_skill("a0,dummy,dummy skill,0")
+master.load_piece("0,dummy,mar,phys,a0,p0,40,40,40")
 print master.skill("a0").desc
+print master.piece("0").param.power
 
